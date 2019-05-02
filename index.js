355f08fc0eae244bf74fd8c806c4016666288ddf
Chomtana <chomtana001@gmail.com>
2019-05-02 16:10:49 +0700

    final

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var gooddata = [];
 var baseline = [];
 var isRunning = false;
 
+//var history = [[1,3,2,"Stopped",new Date()]];
+var history = [];
+
 var engine = require('consolidate');
 
 app.set('views', __dirname + '');
@@ -21,10 +24,10 @@ app.get('/uploaddata', (req, res) => {
       var a1 = (currdata[i] - baseline[i]) % 360;
       var a2 = - (360 - a1 ) % 360
       
-      if (a1 < 0) {
+      while (a1 < 0) {
         a1 += 360;
       }
-      if (a2 > 0) {
+      while (a2 > 0) {
         a2 -= 360;
       }
       
@@ -34,6 +37,8 @@ app.get('/uploaddata', (req, res) => {
       if (Math.abs(a2)>=357 || Math.abs(a2)<=3) {
         a1 = 0;
       }
+      
+      
       
       if (Math.abs(currdata[i])<=5) {
         if (Math.abs(a1) < Math.abs(a2)) {
@@ -83,14 +88,41 @@ app.get('/start', (req, res) => {
 }) 
 
 app.get('/stop', (req, res) => {
+  if (isRunning) {
+    gooddata[4] = (new Date()).toLocaleString();
+    history.push(gooddata);
+  }
+  
   isRunning = false;
   res.send("Stopped");
   
   console.log("Stopped");
 }) 
 
-app.get('/monitor', (req, res) => {
+app.get('/', (req, res) => {
   res.render("./monitor.html");
 }) 
+
+app.get('/history.png', (req, res) => {
+  res.sendFile(__dirname+'/history.png')
+})
+
+app.get('/history', (req,res) => {
+  res.send(history)
+})
+
+app.get('/iotlevel.csv', (req,res) => {
+  res.setHeader('Content-disposition', 'attachment; filename=iotlevel.csv');
+  res.set('Content-Type', 'text/csv');
+  
+  var header = ['Time','Axis 1','Axis 2'];
+  
+  var data = header.join(",");
+  for(var x of history) {
+    data += "\n"+x[4]+","+x[0]+","+x[1];
+  }
+  
+  res.send(data)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
